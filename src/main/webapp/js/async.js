544d27e5c9c40d743354eef0a68b449c35756709
mincong <i@congm.in>
2015-12-13 14:48:08 +0800

    管理员申请、登录接口 - OK

---
+++ src/main/webapp/js/async.js
@@ -0,0 +1,59 @@
+//所有提交可回车实现
+$(".ccapInput").keydown(function(e){
+    if (e.which == 13) {
+        $(this).parent(".ibox").find("button[type='submit']").click();
+    }
+});
+//申请博客
+$(".toapply").on("click", function(){
+    $.ajax({
+        type: "POST",
+        url: "http://host.congm.in:8080/cblog/admins/register",
+        dataType: "json",
+        data: {
+            "username": $("#applyAdminName").val(),
+            "clazz.clazzName": $("#applyClassName").val(),
+            "passwd": $("#applyPassword").val()
+        },
+        success: function(data){
+            if(!data.success){
+                var error_text = "申请失败：";
+                var error = [];
+                $.each(data, function(i, e){
+                    error.push(e);
+                });
+                error_text += error;
+                alert(error_text);
+            }else{
+                alert("申请成功！请以管理员用户方式登录。");
+                window.location.href = "index.html#adminLogin";
+            }
+        }
+    });
+});
+
+//管理员登录
+$(".toadminlogin").on("click", function(){
+    $.ajax({
+        type: "POST",
+        url: "http://host.congm.in:8080/cblog/admins/login",
+        dataType: "json",
+        data: {
+            "username": $("#adminName").val(),
+            "passwd": $("#adminPassword").val()
+        },
+        success: function(data){
+            if(!data.success){
+                var error_text = "登录失败：";
+                var error = [];
+                $.each(data, function(i, e){
+                    error.push(e);
+                });
+                error_text += error;
+                alert(error_text);
+            }else{
+                window.location.href = "admin.html";
+            }
+        }
+    });
+});
